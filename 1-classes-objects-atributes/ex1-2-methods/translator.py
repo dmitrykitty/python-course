64eb683e9a77f46f9c9b3a38314bd06845f37305
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-04-06 22:06:13 +0200

    translatro class added

---
+++ 1-classes-objects-atributes/ex1-2-methods/translator.py
@@ -0,0 +1,37 @@
+class Translator:
+
+    def add(self, eng, rus):
+        if not hasattr(self, 'dictionary'):
+            self.dictionary = {}
+        if eng in self.dictionary.keys():
+            self.dictionary[eng].append(rus)
+        else:
+            self.dictionary[eng] = [rus]
+
+    def remove(self, eng):
+        if eng in self.dictionary.keys():
+            del self.dictionary[eng]
+
+    def translate(self, eng):
+        if eng in self.dictionary.keys():
+            return self.dictionary[eng]
+
+    def show_all(self):
+        for word in self.dictionary.keys():
+            print(f"{word}: {", ".join(self.dictionary[word])}")
+
+
+tr = Translator()
+tr.add("tree", "дерево")
+tr.add("car", "авто")
+tr.add("car", "машина")
+tr.add("leaf", "лист")
+tr.add("go", "идти")
+tr.add("go", "ехать")
+tr.add("go", "ходить")
+
+tr.show_all()
+
+print(*tr.translate("go"))
+tr.remove("leaf")
+tr.show_all()
